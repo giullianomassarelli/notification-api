7c8e56b8e2d7a284070ad68dcb06e3dcc342ecbe
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2023-01-11 08:52:48 -0300

    :tada: Initial commit

---
+++ src/test/java/br/com/gd/notificationapi/NotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gd.notificationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
