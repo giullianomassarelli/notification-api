868eb3fc3dc104378f17e3f865e76957403281b9
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2023-01-13 09:32:06 -0300

    :sparkles: feat: add annations to exception handler

--- src/main/java/br/com/gd/notificationapi/exceptions/SheetException.java
+++ src/main/java/br/com/gd/notificationapi/exceptions/SheetException.java
@@ -1,7 +1,13 @@
 package br.com.gd.notificationapi.exceptions;
 
 import br.com.gd.notificationapi.exceptions.enums.SheetEnum;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@Builder
+@EqualsAndHashCode(callSuper = false)
 public class SheetException extends NotificationApiException{
 
     private static final long serialVersionUID = -4589179341768493322L;
