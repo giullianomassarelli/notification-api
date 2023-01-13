3d5aa92aa8f3858898e297f5f63fce7c917a7e4d
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2023-01-13 11:37:19 -0300

    :sparkles: feat: inject notification service to send email

--- src/main/java/br/com/gd/notificationapi/facades/impl/SheetFacadeImpl.java
+++ src/main/java/br/com/gd/notificationapi/facades/impl/SheetFacadeImpl.java
@@ -5,6 +5,7 @@ import br.com.gd.notificationapi.entities.SheetEntity;
 import br.com.gd.notificationapi.exceptions.SheetException;
 import br.com.gd.notificationapi.exceptions.enums.SheetEnum;
 import br.com.gd.notificationapi.facades.SheetFacade;
+import br.com.gd.notificationapi.services.NotificationService;
 import br.com.gd.notificationapi.services.SheetService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,22 +21,33 @@ public class SheetFacadeImpl implements SheetFacade {
     @Autowired
     private SheetService sheetService;
 
+    @Autowired
+    private NotificationService notificationService;
+
 
     @Override
     public SheetResponseDTO importSheet(MultipartFile file) {
         SheetResponseDTO sheetResponseDTO = new SheetResponseDTO(false);
 
         if (sheetService.validateExcelFile(file)) {
+
             try {
-                sheetService.importSheet(file.getInputStream());
+                List<SheetEntity> sheetEntityList = sheetService.importSheet(file.getInputStream());
+                for (SheetEntity sheetEntity : sheetEntityList) {
+                    if (sheetEntity.getAmount() < 0) {
+                        notificationService.sendEmail(sheetEntity);
+                    }
+                }
                 sheetResponseDTO.setSuccess(true);
+
+                return sheetResponseDTO;
+
             } catch (IOException e) {
                 throw new SheetException(SheetEnum.ERROR_WHEN_IMPORT_SHEET);
             }
         }
         return sheetResponseDTO;
     }
-
     @Override
     public List<SheetEntity> getAll() {
         return sheetService.getAll();
